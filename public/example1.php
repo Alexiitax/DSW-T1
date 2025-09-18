369a4af746b286bedb654b014332f21a5828e31b
Alexiitax <alexiita.ull@gmail.com>
2025-09-18 17:42:31 +0000

    First Example

---
+++ public/example1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Examples</title>
+</head>
+<body>
+    <h1>Example 1</h1>
+    <?php
+        echo "<h2>Hello World\n</h2>";
+    ?>
+    <h3>
+        <?php
+            echo "Testing things";
+        ?>
+    </h3>
+    <?php
+        $name = "Alexa";
+        echo "Your name is $name";
+    ?>
+    <p>Repeat your name: </p>
+    <?php
+        echo "My name is $name";
+        // This is a comment
+
+        /* Those are big
+        comment lines */
+
+        // echo "something commented CTRL + ç".
+    ?>
+</body>
+</html>
